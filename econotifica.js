a05c67b690761183eb5c89b381b51408c6cf75b9
Aluno <aluno@debian.ifrn.local>
2025-06-27 14:35:55 -0300

    Ajustes

--- econotifica.js
+++ econotifica.js
@@ -1,26 +1,28 @@
 Create table pessoa (email varchar primary key,
-tipo varchar, 
-login varchar, 
-senha varchar, 
-bairro varchar);
+    tipo varchar, 
+    login varchar, 
+    senha varchar, 
+    bairro varchar
+);
 
 create table cidade (id serial primary key, 
-estado varchar,
-pais varchar,
-cep varchar,
-fkpessoa varchar,
-FOREIGN KEY (fkpessoa) REFERENCES pessoa (email)
+    estado varchar,
+    pais varchar,
+    cep varchar,
+    fkpessoa varchar,
+    FOREIGN KEY (fkpessoa) REFERENCES pessoa (email)
 );
 
 create table lixeira (id serial primary key,
-nome varchar,
-situacao varchar,
-ip VARCHAR(45),
-latitude DECIMAL(9,6),
-longitude DECIMAL(9,6));
+    nome varchar,
+    situacao varchar,
+    ip VARCHAR(45),
+    latitude DECIMAL(9,6),
+    longitude DECIMAL(9,6)
+);
 
 
 create table grupo (id serial primary key, pessoa varchar, lixeira int,
-FOREIGN KEY (pessoa) REFERENCES pessoa (email),
-FOREIGN KEY (lixeira) REFERENCES lixeira (id)
-)
+    FOREIGN KEY (pessoa) REFERENCES pessoa (email),
+    FOREIGN KEY (lixeira) REFERENCES lixeira (id)
+);
